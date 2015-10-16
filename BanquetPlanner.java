935993372ce093a3663d35da3fe6421b04c887a3
Adam Kurkiewicz <adam@kurkiewicz.pl>
2015-10-16 18:05:22 +0100

    submitted solution

--- BanquetPlanner.java
+++ BanquetPlanner.java
@@ -7,17 +7,24 @@
  */
 
 import java.io.File;
+import java.io.OutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 import org.chocosolver.solver.*;
 import org.chocosolver.solver.variables.*;
 import org.chocosolver.solver.constraints.*;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class BanquetPlanner {
     Solver solver;
     int nGuests;
     int mTables;
     int tableSize;
+    public static OutputStream p = System.out;
+    IntVar tableSizeIV;
+    IntVar[] guests;
+ 
 
     //
     // constrained variables declared here
@@ -28,19 +35,23 @@ public class BanquetPlanner {
             nGuests   = sc.nextInt(); // number of guests
             mTables   = sc.nextInt(); // number of tables
             tableSize = nGuests / mTables;
+            
             solver    = new Solver("banquet planner");
-
-	    //
-            // create the constrained variables here
-	    //
-
+            tableSizeIV = VF.enumerated("tableSize", new int[]{tableSize}, solver);
+            
+            guests = VariableFactory.enumeratedArray("guests", nGuests, 0, mTables, solver);
+            for (int i = 0; i < mTables; i++) {
+                solver.post(ICF.count(i, guests, tableSizeIV));
+            }
             while (sc.hasNext()) {
                 String s = sc.next();
                 int i    = sc.nextInt();
                 int j    = sc.nextInt();
-		//
-                // post constraints for together and apart
-		//
+                if(s.equals("together")){
+                    solver.post(ICF.arithm(guests[i], "=", guests[j]));
+                } else if (s.equals("apart")) {
+                    solver.post(ICF.arithm(guests[i], "!=", guests[j]));
+                }
             }
         }
 
@@ -54,10 +65,31 @@ public class BanquetPlanner {
     }
 
     void result() {
-	//
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        for (int i = 0; i < mTables; i++){
+            hm.put(i, new ArrayList<Integer>());
+        }
+        for (int i = 0; i < nGuests; i++){
+            hm.get(guests[i].getValue()).add(i);
+        }
+        for(int i = 0; i < mTables; i++){
+           System.out.print(i + " ");
+           ArrayList<Integer> guestsAtTable = hm.get(i);
+           for(int j = 0; j < guestsAtTable.size() - 1; j++){
+              System.out.print(guestsAtTable.get(j) + " ");
+           }
+           System.out.print(guestsAtTable.get(guestsAtTable.size() - 1) + "\n");
+           
+        }
+
+        //for (IntVar guest: guests){
+        //    System.out.println(guestNo++ + " " + guest.getValue());
+        //}
+        //
 	// print out solution in specified format (see readme.txt)
 	// so that results can be verified
 	//
+            //stats();
     }
 
     void stats() {
